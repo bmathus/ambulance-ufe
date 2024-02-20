7158d8d414a8f50101ce07d22dc2a010cc2f6f9b
Matus Bojko <bojko.matus@gmail.com>
2024-02-20 11:33:57 +0100

    ci - publish docker image - este raz

--- src/components/mb-ambulance-wl-list/mb-ambulance-wl-list.tsx
+++ src/components/mb-ambulance-wl-list/mb-ambulance-wl-list.tsx
@@ -19,7 +19,7 @@ export class MbAmbulanceWlList {
           estimatedDurationMinutes: 15,
           condition: 'Kontrola'
       }, {
-          name: 'Bc. August Cézar',
+          name: 'Bc. August Niekto',
           patientId: '10096',
           since: new Date(Date.now() - 30 * 60).toISOString(),
           estimatedStart: new Date(Date.now() + 30 * 60).toISOString(),
