7b980e0d0588b1b41ebff667ad121976d457e14c
Matus Bojko <bojko.matus@gmail.com>
2024-03-30 12:47:54 +0100

    fixing typo

--- src/components/mb-ambulance-wl-list/mb-ambulance-wl-list.tsx
+++ src/components/mb-ambulance-wl-list/mb-ambulance-wl-list.tsx
@@ -21,10 +21,10 @@ export class MbAmbulanceWlList {
       if (response.status < 299) {
         return response.data;
       } else {
-        this.errorMessage = `Cannot retrievew list of waiting patients: ${response.statusText}`;
+        this.errorMessage = `Cannot retrieve list of waiting patients: ${response.statusText}`;
       }
     } catch (err: any) {
-      this.errorMessage = `Cannot retrievew list of waiting patients: ${err.message || 'unknown'}`;
+      this.errorMessage = `Cannot retrieve list of waiting patients: ${err.message || 'unknown'}`;
     }
     return [];
   }
